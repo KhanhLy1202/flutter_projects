23f7a3fa6dd38fa1c62e0b47a765900f0d5d673d
llckumadevelop <kumauser01@kuma.ink>
2025-05-06 16:38:11 +0700

    add rolldiceapp

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  GradientContainer.purple({super.key})
+      : color1 = Color.fromARGB(255, 20, 8, 88),
+        color2 = Color.fromARGB(255, 33, 11, 114);
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color1,
+            color2,
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
